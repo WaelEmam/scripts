69a7caab8ff8f7ee46c46c02a3de71c2fd179594
WaelEmam <wael.emam@gmail.com>
2018-06-29 05:07:34 -0400

    Add files via upload

---
+++ dns_check_all_cluster.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -x
+DATE=`date +%Y-%m-%d.%H:%M:%S`
+file="watch.out"
+AMBARI_USER=admin
+AMBARI_PASSWD=admin
+AMBARI_HOST=wemam-lab-1.wael.hortonworks.com
+CLUSER_NAME=wemam_lab
+
+#usage(){
+#        echo "Usage: ./trace.sh <Host FQDN>"
+#        exit 1
+#}
+
+#if [ $# -eq 0 ]
+#  then
+#    usage
+#fi
+
+
+if [ -f "$file" ]
+then
+gzip -S ".$DATE.gz" $file
+fi
+
+while true
+do
+
+for i in $(curl -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By: ambari' -X GET "http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSER_NAME/hosts"| grep host_name| egrep -v "%"| awk -F: '{print $2}' | awk -F \" '{print $2}')
+do
+  echo "====================================" >> watch.out 2>&1
+  echo $i  >> watch.out 2>&1
+  nslookup $i  >> watch.out 2>&1
+  echo "===================================="  >> watch.out 2>&1
+  sleep 5
+  echo $'\n' >> watch.out 2>&1
+  
+  count=$(egrep "server can't" watch.out | wc -l)
+  
+  if [ $count -gt 0 ]
+  then
+    echo "connection issue count:" ${count}
+  fi
+done
+
+done
+~
