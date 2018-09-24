d54004a5f8a8c25cabad4a9b95c5f5bd91ba8fbc
wemam <wael.emam@gmail.com>
2018-09-24 04:34:47 -0700

    add messages

--- update_hosts_file.sh
+++ update_hosts_file.sh
@@ -5,7 +5,9 @@ echo "Enter cluster name:"
 read cluster
 echo "An IP address from the cluster:"
 read IP
+echo "We use sudo, so you might be asked for your computer password"
 sudo sed -i '' "/${cluster}/d" /etc/hosts
+echo "Enter remote server root password"
 ssh -t root@${IP} 'grep c1150 /etc/hosts' > ${cluster}_hosts
 #echo "" | sudo tee -a /etc/hosts > /dev/null
 echo "# ${cluster} Hosts" | sudo tee -a  /etc/hosts
