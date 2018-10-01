d509f55f8ce598d9abd3186a613c50b03efdba7a
wemam <wael.emam@gmail.com>
2018-10-01 11:45:26 -0400

    fix cluster variable in grep

--- update_hosts_file.sh
+++ update_hosts_file.sh
@@ -8,7 +8,7 @@ read IP
 echo "We use sudo, so you might be asked for your computer password"
 sudo sed -i '' "/${cluster}/d" /etc/hosts
 echo "Enter remote server root password"
-ssh -t root@${IP} 'grep c1150 /etc/hosts' > ${cluster}_hosts
+ssh -t root@${IP} "grep $cluster /etc/hosts" > ${cluster}_hosts
 #echo "" | sudo tee -a /etc/hosts > /dev/null
 echo "# ${cluster} Hosts" | sudo tee -a  /etc/hosts
 cat ${cluster}_hosts | sudo tee -a  /etc/hosts
